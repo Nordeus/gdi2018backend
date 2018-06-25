beca5750c86b682cbc0f79374f1e04925758ec1b
Petar Dobrota <petard@nordeus.com>
2018-06-25 08:58:40 +0200

    create project stub

---
+++ src/main/java/rs/sga/gdi18/GameServer.java
@@ -0,0 +1,13 @@
+package rs.sga.gdi18;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GameServer {
+
+	private static final Logger LOG = LoggerFactory.getLogger(GameServer.class.getName());
+
+	public static void main(String[] args) {
+		LOG.debug("Hello world.");
+	}
+}
